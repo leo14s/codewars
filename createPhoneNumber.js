8bd6f15aa4d2d0b90fadca713133308a1103d5e8
Leonardo Veiga <leonardo.veiga@nexxera.com>
2019-09-06 17:48:23 -0300

    outras respostas create phone number

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -11,3 +11,23 @@ function createPhoneNumber(numbers) {
   }
   return number;
 }
+
+function createPhoneNumber(numbers) {
+  return numbers.reduce((p, c) => p.replace("x", c), "(xxx) xxx-xxxx");
+}
+
+function createPhoneNumber(numbers) {
+  numbers = numbers.join("");
+  return (
+    "(" +
+    numbers.substring(0, 3) +
+    ") " +
+    numbers.substring(3, 6) +
+    "-" +
+    numbers.substring(6)
+  );
+}
+
+function createPhoneNumber(numbers) {
+  return numbers.join("").replace(/(...)(...)(.*)/, "($1) $2-$3");
+}
