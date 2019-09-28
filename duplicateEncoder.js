fa509b89ee8572afc422ad6a0bad9daa4acca6be
Leonardo Veiga <leo14s@gmail.com>
2019-09-27 23:49:25 -0300

    duplicateEncoder

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -1,39 +1,43 @@
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
-}
-function MyduplicateEncode(word){
-    let newString = word.toUpperCase();
-    console.log(newString);
-    let tamanho = word.length;
-    let letra;
-    let count = 0;
-    for(let i=0; i< tamanho; i++){
-      letra = word.charAt(i);
-      for(let j=0; j< tamanho; j++){
-        if(letra == word.charAt(j)){
-          count++;
-          console.log(`letra ${i}: ${count} vezes`)
-        }
+String.prototype.replaceAt = function(index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  );
+};
+function MyduplicateEncode(word) {
+  let newString = word.toUpperCase();
+  console.log(newString);
+  let tamanho = word.length;
+  let letra;
+  let count = 0;
+  for (let i = 0; i < tamanho; i++) {
+    letra = word.charAt(i);
+    for (let j = 0; j < tamanho; j++) {
+      if (letra == word.charAt(j)) {
+        count++;
+        console.log(`letra ${i}: ${count} vezes`);
       }
-      if(count > 1){
-        newString = newString.replaceAt(i,')');
-      }else{
-        newString = newString.replaceAt(i,'(');
-      }
-      count = 0;
     }
-    console.log(newString);
+    if (count > 1) {
+      newString = newString.replaceAt(i, ")");
+    } else {
+      newString = newString.replaceAt(i, "(");
+    }
+    count = 0;
+  }
+  console.log(newString);
 }
-MyduplicateEncode('aba');
-
+MyduplicateEncode("aba");
 
 //solução
-function duplicateEncode(word){
-    return word
-      .toLowerCase()
-      .split('')
-      .map( function (a, i, w) {
-        return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
-      })
-      .join('');
-  }
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function(a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
+console.log(duplicateEncode("aba"));
